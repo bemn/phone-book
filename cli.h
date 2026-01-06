64a79cd79b514c86131d7482be15ee422375bdbe
bemn <93151705+bemn@users.noreply.github.com>
2026-01-07 01:29:05 +0500

    Add functions for cli management

---
+++ cli.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <array>
+#include <iostream>
+#include <optional>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using std::array;
+using std::string;
+using std::vector;
+
+namespace cli {
+inline void clear_terminal() {
+  system("clear");  // TODO: make cross-platform
+}
+
+template <typename T>
+std::optional<T> read_value(const string &msg) {
+  std::cout << msg << ": " << std::flush;
+  std::string line;
+  std::getline(std::cin, line);
+  if (line.empty()) {
+    return std::nullopt;
+  }
+  std::stringstream stream(line);
+  T value;
+  if (stream >> value && stream.eof()) return value;
+  return std::nullopt;
+}
+
+template <size_t N>
+unsigned int menu(const string &title, const array<string, N> &items,
+                  const string &exit_message) {
+  bool error = false;
+  do {
+    clear_terminal();
+    std::cout << " --- " << title << " --- \n";
+    for (int i = 0; i < items.size(); i++) {
+      std::cout << i + 1 << ". " << items[i] << '\n';
+    }
+    std::cout << "0. " << exit_message << std::endl;
+    if (error) {
+      std::cout << "Invalid input, try again." << std::endl;
+    }
+    if (const auto choice = read_value<unsigned int>("Enter your choice")) {
+      if (*choice <= items.size()) {
+        return *choice;
+      }
+    }
+    error = true;
+  } while (true);
+}
+}  // namespace cli
--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
+#include <array>
 #include <iostream>
+#include <optional>
+#include <sstream>
 #include <string>
+#include <vector>
+
+#include "cli.h"
+
+using std::array;
+using std::string;
+using std::vector;
 
 int main() {
-  // главный цикл
-  bool enabled = true;
-  do {
-    std::string line;
-    std::getline(std::cin, line);
-    if (line.empty()) {
-      std::cout << "TODO: empty string warning" << std::endl;
-      continue;
-    }
-  } while (enabled);
-  return 0;
+  std::cout << std::endl
+            << cli::menu("Testing", array<string, 2>{"skibidi", "balls"},
+                            "Leave");
 }
